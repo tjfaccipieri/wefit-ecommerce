7dbc9e5a8c325d5eaaed3c43e87afef172e02eea
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2023-10-03 20:40:12 -0300

    feat: iniciado lógica do carrinho

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,23 +1,37 @@
-import { Link } from "react-router-dom"
-import Button from "../Utils/Button"
-import EmptyCartContainer from "../Utils/CartContainer"
-import Container from "../Utils/Container"
-import emptyCart from '/emptyCart.png'
-import { useStore } from "../../context/StoreContext"
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Film } from "../../App"
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Film } from '../../App';
+import { useStore } from '../../context/StoreContext';
+import Button from '../Utils/Button';
+import EmptyCartContainer, { CartContainer } from '../Utils/CartContainer';
+import Container from '../Utils/Container';
+import emptyCart from '/emptyCart.png';
+import plusIcon from '/plusIcon.png';
+import minusIcon from '/minusIcon.png';
+import trash from '/trash.png';
 
 function Cart() {
-
-  const {cartItems, decreaseQuant} = useStore()
+  const { cartItems, decreaseQuant, increaseQuant, removeItem } = useStore();
 
   const [films, setFilms] = useState<Film[]>([]);
 
-  const selectedIds = cartItems.map(item => item.id)
-  const selectedItems: Film[] = films.filter(item => selectedIds.includes(item.id))
+  // const selectedIds = cartItems.map((item) => item.id);
+  // const selectedItems: Film[] = films.filter((item) =>
+  //   selectedIds.includes(item.id)
+  // );
 
-  console.log(selectedItems);
+  const selectedItemsMap = {};
+  cartItems.forEach((item) => {
+    selectedItemsMap[item.id] = item.qtd;
+  });
+
+  const combinedList = films.map((item) => ({
+    ...item,
+    quantity: selectedItemsMap[item.id] || 0,
+  }));
+
+  const itemsToShow = combinedList.filter((item) => item.quantity !== 0);
 
   async function getData() {
     try {
@@ -35,31 +49,55 @@ function Cart() {
 
   useEffect(() => {
     // getItens()
-  }, [])
+  }, []);
+
+  let total: number = 0
 
   return (
     <Container>
       {cartItems.length === 0 ? (
         <EmptyCartContainer>
-        <h2>Parece que não há nada por aqui :(</h2>
-        <img src={emptyCart} alt="" />
-        <Link to='/'>
-          <Button width={180}><p>Voltar</p></Button>
-        </Link>
-      </EmptyCartContainer>
-      ): (
+          <h2>Parece que não há nada por aqui :(</h2>
+          <img src={emptyCart} alt="" />
+          <Link to="/">
+            <Button width={180}>
+              <p>Voltar</p>
+            </Button>
+          </Link>
+        </EmptyCartContainer>
+      ) : (
         <>
-          {selectedItems.map(item => (
-            <>
-            <p>{item.title}</p>
-            <button onClick={() => decreaseQuant(item.id)}>remove</button>
-            </>
-          ))}
+          <CartContainer>
+            {itemsToShow.map((item) => (
+              <div>
+                <img src={item.image} alt="" />
+                <p className='title'>{item.title}</p>
+                <div className="quantity">
+                  <img src={minusIcon} alt="" onClick={() => decreaseQuant(item.id)} />
+                  <span>{item.quantity}</span>
+                  <img src={plusIcon} alt="" onClick={() => increaseQuant(item.id)} />
+                </div>
+                <p className="price">{(item.price * item.quantity).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
+                <img className='trash' src={trash} onClick={() => removeItem(item.id)} />
+              </div>
+            ))}
+
+            <hr style={{width: '100%'}} />
+
+            <Button>Finalizar compra</Button>
+            <div>
+              Valor total: {itemsToShow.map(item => {
+                {total = total + (item.price * item.quantity)}
+                return <></>
+              })}
+              {total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
+            </div>
+
+          </CartContainer>
         </>
-  )
-}
+      )}
     </Container>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
--- src/components/Utils/CartContainer.ts
+++ src/components/Utils/CartContainer.ts
@@ -24,6 +24,48 @@ const EmptyCartContainer = styled.main`
 
 export const CartContainer = styled(EmptyCartContainer)`
   padding: 24px;
+
+  div {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    gap: 8px;
+    justify-content: center;
+
+    img {
+      height: 114px;
+      width: auto;
+    }
+
+    .title {
+      width: 100%;
+    }
+
+    .quantity {
+      display: flex;
+      height: fit-content;
+      justify-content: center;
+      gap: 4px;
+
+      span {
+        border: 1px solid #D9D9D9;
+        width: 62px;
+        text-align: center;
+        border-radius: 4px;
+        font-size: 14px;
+        font-weight: 400;
+      }
+
+      img {
+        height: 100%;
+      }
+    }
+
+    .trash {
+      height: 100%;
+      cursor: pointer;
+    }
+  }
   `
 
 export default EmptyCartContainer
